ccb6d24ffd4a4e50e9ac2b70b919eaca95163e04
Sai <pss2k5@gmail.com>
2024-10-20 12:06:38 +0530

    day 6...modify.

--- detail-script.js
+++ detail-script.js
@@ -119,7 +119,7 @@ document.getElementById("submitBooking").addEventListener("click", async () => {
 
   try {
     const response = await fetch(
-      "https://ticketstie-backend.onrender.com/movie/get-movies",
+      "https://ticketstie-backend.onrender.com/movie/book-movie",
       {
         method: "POST",
         headers: {
